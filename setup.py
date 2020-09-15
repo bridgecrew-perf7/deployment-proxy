baefcdfdef1aeaf9e5416d6ebf24194e972dda8b
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-15 09:21:41 -0600

    things

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dproxy",
-    version="0.0.9",
+    version="0.1.0",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Proxy API",
