4eca39a0dd86aef1f094edcf880514a1ebce0f97
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-16 06:15:55 -0600

    things

--- dproxy/app.py
+++ dproxy/app.py
@@ -9,6 +9,7 @@ from dproxy.controllers.healthcheck import get_healthcheck
 from dproxy.controllers.server import patch_server, post_server_history
 
 import requests
+import connexion
 from flask import Flask, request
 
 
@@ -28,52 +29,9 @@ if not Config.TOKEN:
         with open("/etc/default/dproxy", "a") as file:
             file.write("TOKEN={}".format(resp["token"]))
 
-
-app = Flask(__name__)
+flask_app = connexion.FlaskApp(__name__)
+flask_app.add_api("openapi.yaml", validate_responses=True, strict_validation=True)
+app = flask_app.app
 app.config.from_object(Config)
-
-
-@app.route("/", methods=["GET", "POST"])
-def healthcheck():
-    if request.method == "GET":
-        return get_healthcheck()
-
-
-@app.route("/rollout", methods=["POST"])
-def rollout():
-    if request.method == "POST":
-        return post_rollout()
-
-
-@app.route("/rollback", methods=["POST"])
-def rollback():
-    if request.method == "POST":
-        return post_rollback()
-
-
-@app.route("/server", methods=["PATCH"])
-def server():
-    if request.method == "PATCH":
-        return patch_server()
-
-
-@app.route("/server/history", methods=["POST"])
-def server_history():
-    if request.method == "POST":
-        return post_server_history()
-
-
-@app.route("/versionlock", methods=["POST"])
-def versionlock():
-    if request.method == "POST":
-        return post_versionlock()
-
-
-@app.route("/update", methods=["POST"])
-def update():
-    if request.method == "POST":
-        return post_update()
-    
-
 with app.app_context():
     runner = make_runner(app)
