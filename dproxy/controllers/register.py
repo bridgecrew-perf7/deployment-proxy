086da9ef6257890a48bd2c4bd7d61ff9653514b7
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-17 08:59:11 -0600

    things

--- dproxy/controllers/register.py
+++ dproxy/controllers/register.py
@@ -7,20 +7,19 @@ from dproxy.config import Config
 
 def post_register_server():
     data = request.get_json()
-    print(data)
-    #try:
-    #r = requests.post(Config.DEPLOYMENT_API_URI + "/register/server", json=data)
-    #resp = r.json()
-    response = {
-        "status": "success",
-        "message": "Server successfully registered",
-        #"TOKEN": resp["TOKEN"]
-    }
-    return response, 200
-    #except Exception as e:
-    #    response = {
-    #        "status": "failure",
-    #        "message": "Register server failed",
-    #        "exception": str(e)
-    #    }
-    #    return response, 409
+    try:
+        r = requests.post(Config.DEPLOYMENT_API_URI + "/register/server", json=data)
+        resp = r.json()
+        response = {
+            "status": "success",
+            "message": "Server successfully registered",
+            "TOKEN": resp["token"]
+        }
+        return response, 200
+    except Exception as e:
+        response = {
+            "status": "failure",
+            "message": "Register server failed",
+            "exception": str(e)
+        }
+        return response, 409
