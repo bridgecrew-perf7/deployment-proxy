7f32bc65f564e84f99c1edc18253701890f8382d
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-21 20:58:59 -0600

    trying to get logging working

--- dproxy/controllers/register.py
+++ dproxy/controllers/register.py
@@ -9,20 +9,20 @@ logger = get_logger
 
 def post_register_server():
     data = request.get_json()
-    try:
-        r = requests.post(Config.DEPLOYMENT_API_URI + "/register/server", json=data)
-        logger.debug(r.content)
-        resp = r.json()
-        response = {
-            "status": "success",
-            "message": "Server successfully registered",
-            "token": resp["token"]
-        }
-        return response, 200
-    except Exception as e:
-        response = {
-            "status": "failure",
-            "message": "Register server failed",
-            "exception": str(e)
-        }
-        return response, 409
+    #try:
+    r = requests.post(Config.DEPLOYMENT_API_URI + "/register/server", json=data)
+    logger.info(r.content)
+    resp = r.json()
+    response = {
+        "status": "success",
+        "message": "Server successfully registered",
+        "token": resp["token"]
+    }
+    return response, 200
+   # except Exception as e:
+   #     response = {
+   #         "status": "failure",
+   #         "message": "Register server failed",
+   #         "exception": str(e)
+   #     }
+   #     return response, 409
