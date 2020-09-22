4010972c5f1fdd6e2660209f8d81dd87dc07fb0e
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-22 04:03:52 -0600

    fix states for register server

--- dproxy/controllers/register.py
+++ dproxy/controllers/register.py
@@ -4,25 +4,25 @@ from flask import request
 
 from dproxy.config import Config
 from dproxy.config import get_logger
-logger = get_logger
+logger = get_logger()
 
 
 def post_register_server():
     data = request.get_json()
-    #try:
-    r = requests.post(Config.DEPLOYMENT_API_URI + "/register/server", json=data)
-    logger.info(r.content)
-    resp = r.json()
-    response = {
-        "status": "success",
-        "message": "Server successfully registered",
-        "token": resp["token"]
-    }
-    return response, 200
-   # except Exception as e:
-   #     response = {
-   #         "status": "failure",
-   #         "message": "Register server failed",
-   #         "exception": str(e)
-   #     }
-   #     return response, 409
+    try:
+        r = requests.post(Config.DEPLOYMENT_API_URI + "/register/server", json=data)
+        logger.info(r.content)
+        resp = r.json()
+        response = {
+            "status": "success",
+            "message": "Server successfully registered",
+            "token": resp["token"]
+        }
+        return response, 200
+    except Exception as e:
+        response = {
+            "status": "failure",
+            "message": "Register server failed",
+            "exception": str(e)
+        }
+        return response, 409
