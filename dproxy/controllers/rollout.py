c34ae52ae64d98a3aca94addf7e059feec531f87
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-24 07:20:24 -0600

    check for sting instead of int in rollout deployment_id

--- dproxy/controllers/rollout.py
+++ dproxy/controllers/rollout.py
@@ -4,11 +4,12 @@ from flask import request
 
 from dproxy.config import Config
 from dproxy.tasks.deployment.tasks import rollout
+from dproxy.config import get_logger
+logger = get_logger()
 
 
 def post_rollout():
     data = request.get_json()
-
     try:
         rollout.apply_async(args=[data])
         response = {
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dproxy",
-    version="0.2.34",
+    version="0.2.36",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Proxy API",
