8de5d99da5fe17896abc0af8ae774186ba7d665b
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-24 07:25:11 -0600

    check for sting instead of int in rollout deployment_id

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dproxy",
-    version="0.2.38",
+    version="0.2.39",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Proxy API",
