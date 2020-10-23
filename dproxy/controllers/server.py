56271111f0356d2691f3f586c8a420765d88a521
Jacob <jacob.matthew.bushman@endurance.com>
2020-10-23 16:39:04 -0600

    fix server history

--- dproxy/controllers/server.py
+++ dproxy/controllers/server.py
@@ -20,7 +20,6 @@ def patch_server():
 def post_server_history():
     data = request.get_json()
     cookies = {"access_token_cookie": request.headers["Authorization"]}
-    http = get_http
-    r = http.post(f"{Config.DEPLOYMENT_API_URI}/server/history", cookies=cookies, json=data)
+    r = requests.post(f"{Config.DEPLOYMENT_API_URI}/server/history", cookies=cookies, json=data)
     resp = r.json()
     return resp, r.status_code
