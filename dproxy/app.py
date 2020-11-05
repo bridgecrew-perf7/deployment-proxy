eeaabf3d738158a4f277c0ffaea71ce207c8f91c
Jason Moehlman <jason.moehlman@endurance.com>
2020-11-05 07:23:22 -0500

    minor cleanup before merge

--- dproxy/app.py
+++ dproxy/app.py
@@ -4,20 +4,14 @@ from dproxy.tasks.runner import make_runner
 from dproxy.util.core import set_state, register_proxy
 
 import os
-import requests
 import connexion
-from dotenv import load_dotenv
-from flask import Flask, request
 
 import logging.handlers
 
-# These should be in .env / Config object.
-MAX_BYTES = 10000
-BACKUP_COUNT = 10
-
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s")
 
-rotating_log_handeler = logging.handlers.RotatingFileHandler(Config.LOG_FILE,maxBytes=int(Config.LOG_MAX_BYTES),backupCount=int(Config.LOG_BACKUP_COUNT))
+rotating_log_handeler = logging.handlers.RotatingFileHandler(Config.LOG_FILE, maxBytes=int(Config.LOG_MAX_BYTES),
+                                                             backupCount=int(Config.LOG_BACKUP_COUNT))
 rotating_log_handeler.setFormatter(formatter)
 rotating_log_handeler.setLevel(logging.DEBUG)
 logging.getLogger('').addHandler(rotating_log_handeler)
