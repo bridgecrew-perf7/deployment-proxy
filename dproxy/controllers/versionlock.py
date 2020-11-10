28c656129af33aff575ab391f35a362e53d66c89
Jacob <jacob.matthew.bushman@endurance.com>
2020-11-10 11:21:42 -0700

    things

--- dproxy/controllers/versionlock.py
+++ dproxy/controllers/versionlock.py
@@ -8,14 +8,14 @@ def post_versionlock():
     try:
         if "hostname" in data:
             r = requests.post(
-                f"http://{data['hostname']}:{data['port']}/versionlock", json=data
+                f"http://{data['hostname']}:{data['port']}/api/v1/versionlock", json=data
             )
             resp = r.json()
             return resp, 201
         else:
             response = {
                 "status": "failed",
-                "message": "Unable to find Base64 encoded client url.",
+                "message": "Unable to find hostname.",
             }
             return response, 409
     except Exception as e:
@@ -25,7 +25,7 @@ def post_versionlock():
 def get_versionlock(hostname, port):
     try:
         http = get_http
-        r = http.get(f"http://{hostname}:{port}/versionlock", json=data)
+        r = http.get(f"http://{hostname}:{port}/api/v1/versionlock", json=data)
         resp = r.json()
         return resp, 201
     except Exception as e:
