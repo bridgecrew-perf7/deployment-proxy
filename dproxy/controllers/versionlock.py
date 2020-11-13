148765c887624c173a8d6c99b0d30cf168528762
Jacob <jacob.matthew.bushman@endurance.com>
2020-11-13 10:38:57 -0700

    remove post versionlock

--- dproxy/controllers/versionlock.py
+++ dproxy/controllers/versionlock.py
@@ -3,25 +3,6 @@ from flask import request
 from dproxy.util.http_helper import get_http
 
 
-def post_versionlock():
-    data = request.get_json()
-    try:
-        if "hostname" in data:
-            r = requests.post(
-                f"http://{data['hostname']}:{data['port']}/api/v1/versionlock", json=data
-            )
-            resp = r.json()
-            return resp, 201
-        else:
-            response = {
-                "status": "failed",
-                "message": "Unable to find hostname.",
-            }
-            return response, 409
-    except Exception as e:
-        raise Exception(f"Unable to Post versionlock: {e}")
-
-
 def get_versionlock(hostname, port):
     try:
         http = get_http
