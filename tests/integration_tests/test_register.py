c791e4b38a6224cb103e003813cbaf2d85e1d6bd
Ritesh Gupta <ritesh.gupta@endurance.com>
2020-11-19 18:00:08 -0700

    Fixed tests

--- tests/integration_tests/test_register.py
+++ tests/integration_tests/test_register.py
@@ -1,18 +1,19 @@
 import requests
 
 
-def test_post_register_server(proxy_url):
+def test_post_register_server(proxy_url, proxy_hostname):
     data = {
         "hostname": "my3.test.unifiedlayer.com",
         "ip": "3.9.112.22",
         "port": "8003",
-        "api_version": "v1",
-        "created_by": "twest_client",
+        "version": "v1",
+        "protocol": "http",
+        "created_by": "test_client",
         "state": "NEW",
-        "group": "hp_testw",
+        "group": "hp_test",
         "environment": "alpha",
         "location": "test",
-        "deployment_proxy": "localhost.localdomain",
+        "deployment_proxy": proxy_hostname,
     }
     response = requests.post(proxy_url + "/register", json=data)
     print(response.text)
